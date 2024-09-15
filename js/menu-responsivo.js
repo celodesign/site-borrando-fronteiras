a790c94b6ee0e35cc4beb4acc098b171410bfb54
Marcelo Lins <marcelolins.designer@gmail.com>
2024-09-15 19:40:40 -0300

    ajustes

---
+++ js/menu-responsivo.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+  $(".main-menu").click(function(){
+    const larguraTela = screen.width;
+    if(!$(this).hasClass("actives")){
+      $("#aside").addClass("main-menu-open");
+      $(this).addClass("actives");
+      $(".main-menu").css("right", "71%")  
+      if(larguraTela == 320){
+        $(".main-menu").css("right", "67%");
+      }
+    }else{
+      $("#aside").removeClass("main-menu-open");
+      $(this).removeClass("actives");   
+      if(larguraTela == 320){
+        $(".main-menu").css("right", "190%");
+      }else if(larguraTela == 768) {
+        $(".main-menu").css("right", "115%");
+      }else{
+        $(".main-menu").css("right", "165%");
+      }
+    }
+    console.log(larguraTela)
+  });
+  
+});
+
+$(document).ready(function(){
+  $(".act-start").click(function(){
+    const larguraTela = screen.width;
+    if(!$(".main-menu").hasClass("act")){
+        $("#aside").addClass("main-menu-open");
+        $(".main-menu").addClass("actives");
+        $(".main-menu").css("right", "71%");
+        if(larguraTela == 320){
+        $(".main-menu").css("right", "67%");
+      }  
+    }else{
+        $("#aside").removeClass("main-menu-open");
+        $(".main-menu").removeClass("actives");
+        $(".main-menu").css("right", "165%")
+        if(larguraTela == 320){
+        $(".main-menu").css("right", "190%");
+      }else if(larguraTela == 768) {
+        $(".main-menu").css("right", "115%");
+      }else{
+        $(".main-menu").css("right", "165%");
+      }
+    }
+  });
+  
+});
