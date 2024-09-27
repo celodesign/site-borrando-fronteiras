7c6ac0dc3e69fe209c82f390e3c9a9621e8c05f9
Marcelo Lins <marcelolins.designer@gmail.com>
2024-09-27 01:33:59 -0300

    site borrando second version

--- js/menu-responsivo.js
+++ js/menu-responsivo.js
@@ -1,60 +1,22 @@
-$(document).ready(function(){
-  $(".main-menu").click(function(){
-    const larguraTela = screen.width;
-    if(!$(this).hasClass("actives")){
-      $("#aside").addClass("main-menu-open");
-      $(this).addClass("actives");
-      $(".main-menu").css("right", "71%")  
-      if(larguraTela == 320){
-        $(".main-menu").css("right", "67%");
-      }else if(larguraTela == 768) {
-        $(".main-menu").css("right", "61%");
-      }else{
-        $(".main-menu").css("right", "71%");
-      }  
-    }else{
-      $("#aside").removeClass("main-menu-open");
-      $(this).removeClass("actives");   
-      if(larguraTela == 320){
-        $(".main-menu").css("right", "190%");
-      }else if(larguraTela == 768) {
-        $(".main-menu").css("right", "115%");
-      }else{
-        $(".main-menu").css("right", "165%");
-      }
-    }
+function menuShow(){
+  let menuMobile = document.querySelector('.main-menu');
+  let areaMenu = document.querySelector('#area-menu');
+  if(menuMobile.classList.contains('actives')){
+    menuMobile.classList.remove('actives');
+    menuMobile.classList.remove('icon-open');
+    areaMenu.classList.remove('open');
+  }else{
+    menuMobile.classList.add('actives');
+    menuMobile.classList.add('icon-open');
+    areaMenu.classList.add('open');
+  }
+}
 
-  });
-  
-});
-
-$(document).ready(function(){
-  $(".act-start").click(function(){
-    const larguraTela = screen.width;
-    if(!$(".main-menu").hasClass("act")){
-        $("#aside").addClass("main-menu-open");
-        $(".main-menu").addClass("actives");
-        $(".main-menu").css("right", "71%");
-        if(larguraTela == 320){
-        $(".main-menu").css("right", "67%");
-        }else if(larguraTela == 768) {
-          $(".main-menu").css("right", "115%");
-        }else{
-          $(".main-menu").css("right", "71%");
-        }  
-    }else{
-        $("#aside").removeClass("main-menu-open");
-        $(".main-menu").removeClass("actives");
-        $(".main-menu").css("right", "165%")
-        if(larguraTela == 320){
-        $(".main-menu").css("right", "190%");
-        }else if(larguraTela == 768) {
-          $(".main-menu").css("right", "115%");
-        }else{
-          $(".main-menu").css("right", "165%");
-        }
-    }
-
-  });
-  
-});
+function meuShowItens(){
+  let itemMenu = document.querySelector('.item-menu');
+  let menuMobile = document.querySelector('.main-menu');
+  let areaMenu = document.querySelector('#area-menu');
+  menuMobile.classList.remove('actives');
+  menuMobile.classList.remove('icon-open');
+  areaMenu.classList.remove('open');
+}
