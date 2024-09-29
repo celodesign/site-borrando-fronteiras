b76cec19b899c51ea2a948b9ccfe53b967abfc72
Marcelo Lins <marcelolins.designer@gmail.com>
2024-09-29 17:36:48 -0300

    ajuste responsivo

---
+++ js/ajustes-responsivo.js
@@ -0,0 +1,9 @@
+function ajustesResponsivo(){
+    let alturaDoSwiper = document.querySelector('.swiper');
+    let alturaBorrando = alturaDoSwiper.clientHeight;
+    //let itemBorrando = document.querySelector('.img-swiper'); 
+    if(window.innerWidth == 1024 & window.innerHeight == 600){
+      $('.img-swiper').css({'height': alturaBorrando});
+    }
+    console.log(window.innerWidth);
+  };
